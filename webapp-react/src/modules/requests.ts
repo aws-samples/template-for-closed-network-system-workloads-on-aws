001f2eb9125be23be8fd9b3e175cafc174804bd2
Ray Oshikawa <rayohope@amazon.co.jp>
2023-09-20 17:02:54 +0900

    Fix domain

--- webapp-react/src/modules/requests.ts
+++ webapp-react/src/modules/requests.ts
@@ -2,7 +2,7 @@ import React, { useState, useEffect,Dispatch,SetStateAction } from "react";
 import axios from 'axios';
 import { Record } from '../types/types';
 //CHANGE HERE same domain to infra/stage.js
-const endpoint = "https://app.rayohopeapp.local/apigw/";
+const endpoint = "https://app.templateapp.local/apigw/";
 
 export const getFromEndpoint =  async () => {
     const response = await axios.get(endpoint+'sample/');
