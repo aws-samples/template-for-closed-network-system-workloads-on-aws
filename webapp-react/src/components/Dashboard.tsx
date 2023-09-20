c72717edc5d1c0afdeffd21e7c077332cfc3f575
Ray Oshikawa <rayohope@amazon.co.jp>
2023-09-20 16:28:16 +0900

    Fix #16 react webapp request commonize

--- webapp-react/src/components/Dashboard.tsx
+++ webapp-react/src/components/Dashboard.tsx
@@ -3,42 +3,21 @@ import { Record } from "../types/types";
 import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import { RecordForm } from "./Form";
 import { RecordList } from "./List";
-import axios from "axios";
+import { getFromEndpoint } from "../modules/requests";
 
 
-//mui
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import Button from '@mui/material/Button';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import Checkbox from '@mui/material/Checkbox';
 
-//CHANGE HERE same domain to infra/stage.js
-const endpoint = "https://app.templateapp.local/apigw/";
 export const Dashboard: React.FC<{}> = ({}) => {
-    const [samplerecord, setSamplerecord] = React.useState<Record[]>([])
+    const [samplerecords, setSamplerecords] = React.useState<Record[]>([])
     useEffect(()=>{
-        axios.get(endpoint+'sample/')
-        .then(function (response) {
-            console.log("response:",response)
-            const records=response.data as Record[];
-            setSamplerecord(records);
-        });
+        getFromEndpoint().then((res)=>setSamplerecords(res));
     },[]);
     return(<React.Fragment>
         <HashRouter>
-        <h1>Hello From S3 through CodePipeline ! </h1><br/>
-
-                
+        <h1>Hello From S3 through CodePipeline ! </h1><br/>            
                 <Routes>
-                    <Route path="/" Component={({}) => {return <RecordList records={samplerecord}/>}} />
-                    <Route path="/sampleapp/form" Component={({}) => {return <RecordForm records={samplerecord} setRecords={setSamplerecord}  endpoint={endpoint}/>}} />
+                    <Route path="/" Component={({}) => {return <RecordList records={samplerecords}/>}} />
+                    <Route path="/sampleapp/form" Component={({}) => {return <RecordForm records={samplerecords} setRecords={setSamplerecords}/>}} />
                 </Routes>
             
         </HashRouter>
