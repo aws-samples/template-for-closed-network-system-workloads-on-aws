acecb2a8e8ff472e132e5017321f0391a6dfcbac
Yozo Suzuki <suzukyz@amazon.co.jp>
2023-12-11 23:45:19 +0900

    add types for event/result

--- functions/post.ts
+++ functions/post.ts
@@ -1,16 +1,31 @@
 import Connection from "./lib/connect";
 import { Logger } from "@aws-lambda-powertools/logger";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { isBoolean } from "lodash";
 
 const logger = new Logger({ serviceName: "postLambda" });
 
-export const handler = async (event: any): Promise<any> => {
-  const id = event.queryStringParameters.id;
-  const job0001_flag = event.queryStringParameters.job0001_flag;
-  const job0002_flag = event.queryStringParameters.job0002_flag;
-  const job0003_flag = event.queryStringParameters.job0003_flag;
-  const job0004_flag = event.queryStringParameters.job0004_flag;
-  const job0005_flag = event.queryStringParameters.job0005_flag;
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  if (!event.body) {
+    const response = {
+      statusCode: 400,
+      body: JSON.stringify("Body parameter is null"),
+    };
+    logger.error("Body parameter is null");
+    return response;
+  }
+  const body = JSON.parse(event.body);
+  const [
+    id,
+    job0001_flag,
+    job0002_flag,
+    job0003_flag,
+    job0004_flag,
+    job0005_flag,
+  ] = body;
+
   // check if there is data
   if (
     !id ||
@@ -71,7 +86,7 @@ export const handler = async (event: any): Promise<any> => {
     logger.error(e.toString());
     const response = {
       statusCode: 500,
-      body: JSON.stringify(e),
+      body: JSON.stringify("Server error"),
     };
     return response;
   }
